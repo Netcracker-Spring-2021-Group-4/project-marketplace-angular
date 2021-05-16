670dd5c650655b0e158ac9e6a2da9a7ce15bf26a
Fillip Molodtsov <greatdrummer1@gmail.com>
2021-05-16 20:51:11 +0300

    fix for deploy

--- set-env.js
+++ set-env.js
@@ -1,6 +1,7 @@
 const { writeFile } = require('fs');
 // Configure Angular `environment.ts` file path
-const targetPath = './src/environments/environment.prod.ts';
+const targetProdPath = './src/environments/environment.prod.ts';
+const targetBlankPath = './src/environments/environment.ts';
 // Load node modules
 require('dotenv').config();
 // `environment.ts` file structure
@@ -11,12 +12,25 @@ const envConfigFile = `export const environment = {
   captchaKey: '${process.env.CAPTCHA_KEY}'
 };
 `;
+
+const blankConfigFile = `export const environment = {
+  production: false
+};
+`;
 console.log('The file `environment.prod.ts` will be written with the following content: \n');
 console.log(envConfigFile);
-writeFile(targetPath, envConfigFile, function (err) {
+writeFile(targetProdPath, envConfigFile, function (err) {
   if (err) {
     throw console.error(err);
   } else {
-    console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
+    console.log(`Angular environment.prod.ts file generated correctly at ${targetProdPath} \n`);
+  }
+});
+
+writeFile(targetBlankPath, blankConfigFile, function (err) {
+  if (err) {
+    throw console.error(err);
+  } else {
+    console.log(`Angular environment.ts file generated correctly at ${targetBlankPath} \n`);
   }
 });
