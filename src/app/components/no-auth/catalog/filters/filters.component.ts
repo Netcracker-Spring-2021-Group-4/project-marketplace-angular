3fe7c4e11d7b991cb2b7bc393a2cf290d00d3be5
alina-code <alinstart68@gmail.com>
2021-05-31 11:10:22 +0300

    filters look

--- src/app/components/no-auth/catalog/filters/filters.component.ts
+++ src/app/components/no-auth/catalog/filters/filters.component.ts
@@ -13,22 +13,7 @@ export class FiltersComponent implements OnInit {
   categoryList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
 
 
-  minValue: number = 100;
-  maxValue: number = 400;
-  options: Options = {
-    floor: 0,
-    ceil: 500,
-    translate: (value: number, label: LabelType): string => {
-      switch (label) {
-        case LabelType.Low:
-          return '<b>Min price:</b> $' + value;
-        case LabelType.High:
-          return '<b>Max price:</b> $' + value;
-        default:
-          return '$' + value;
-      }
-    }
-  };
+
 
   constructor() { }
 
