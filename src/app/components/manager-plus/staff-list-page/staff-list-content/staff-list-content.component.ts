79160276ccd3f4ea221bd6ffa0cf76fe789aad50
Andrew <verden.kma@gmail.com>
2021-06-02 12:21:38 +0300

    - add edit, delete 'buttons'

--- src/app/components/manager-plus/staff-list-page/staff-list-content/staff-list-content.component.ts
+++ src/app/components/manager-plus/staff-list-page/staff-list-content/staff-list-content.component.ts
@@ -1,6 +1,4 @@
-import {Component, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
-import {UserSearchModel} from "../../../../shared/models/api/send/user-search.model";
-import {StaffSearchHttpService} from "../../../../api-services/staff-search-http.service";
+import {Component, Input, OnInit} from '@angular/core';
 import {ProfileModel} from "../../../../shared/models/api/receive/profile.model";
 
 
@@ -13,7 +11,7 @@ export class StaffListContentComponent implements OnInit {
 
   @Input() matchingStaff: ProfileModel[];
 
-  columnsToDisplay = ['userId', 'email', 'firstName', 'lastName', 'phoneNumber', 'role', 'status'];
+  columnsToDisplay = ['userId', 'email', 'firstName', 'lastName', 'phoneNumber', 'role', 'status', 'actions'];
 
   constructor() {
   }
