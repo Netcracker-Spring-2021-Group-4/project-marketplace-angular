450a5dae034b9884ba6524f9ee7d720c7d59051d
Ivan Mordas <ivanmordas49@gmail.com>
2021-06-04 21:18:12 +0300

    fixed regexp

--- src/app/components/manager-plus/services/product-manager-form.service.ts
+++ src/app/components/manager-plus/services/product-manager-form.service.ts
@@ -36,9 +36,8 @@ export class ProductManagerFormService {
 
 
 }
-const productNameRegExp = "^\p{L}{2}[\\p{L}\\s\\d]{0,28}$"
+const productNameRegExp = "^[^\\d\\s]{2}[\\w\\s]{0,28}$"
 const productName = () => ([null, [Validators.required, Validators.pattern(productNameRegExp)]])
-// const productName = (value?: string) => ([value ?? null, [Validators.required,Validators.minLength(2), Validators.maxLength(30)]])
 const inStock = (value?: number) => ([value ?? null, [Validators.required, Validators.min(1)]])
 const reserved = (value?: number) => ([value ?? null, [Validators.required, Validators.min(1)]])
 const price = (value?: number) => ([value ?? null, [Validators.required, Validators.min(0)]])
