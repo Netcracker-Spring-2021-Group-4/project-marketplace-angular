2cc3b3f98925a6396f7b4b1dc06a11083dd3deae
Ivan Mordas <ivanmordas49@gmail.com>
2021-06-04 19:47:51 +0300

    create component to create product

--- src/app/components/manager-plus/create-product-page/create-product-page.component.ts
+++ src/app/components/manager-plus/create-product-page/create-product-page.component.ts
@@ -1,15 +1,105 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormGroup} from "@angular/forms";
+import {ValidationMessages} from "../../../shared/models/labels/validation.message";
+import {ProductManagerFormService} from "../../manager-plus/services/product-manager-form.service";
+import {Toaster} from "ngx-toast-notifications";
+import {finalize} from "rxjs/operators";
+import Labels from "../../../shared/models/labels/labels.constant";
+import {ManagerApiService} from "../../../api-services/manager-http.service";
+import {HttpErrorResponse} from "@angular/common/http";
+import {Subscription} from "rxjs";
+import {MatSelectChange} from "@angular/material/select";
+import {Categories} from "../../../shared/models/api/receive/categories";
 
 @Component({
   selector: 'app-create-product-page',
   templateUrl: './create-product-page.component.html',
   styleUrls: ['./create-product-page.component.scss']
 })
-export class CreateProductPageComponent implements OnInit {
 
-  constructor() { }
+export class CreateProductPageComponent implements OnInit,OnDestroy {
 
-  ngOnInit(): void {
+  form: FormGroup
+  selectedFile: File
+  categories: Categories[]
+  isLoading = false
+  currentValue:any
+  selected2:any
+  isNotPng: boolean
+  private subscriptions: Subscription[] = []
+
+  productNameErrorMessage =ValidationMessages.productName
+  quantityErrorMessage = ValidationMessages.quantity
+  priceErrorMessage = ValidationMessages.price
+  fileErrorMessage = ValidationMessages.file
+
+  constructor(
+    private productManagerFormService: ProductManagerFormService,
+    private managerApiService: ManagerApiService,
+    private toaster: Toaster,
+  ) {
+    this.form = this.productManagerFormService.createProductForm();
   }
 
+  ngOnInit(): void {
+    this.getCategories();
+  }
+  OnInput(event: MatSelectChange): void {
+    this.currentValue = {
+      value: event.value,
+      text: event.source.triggerValue
+    };
+  }
+
+  public getCategories(): void{
+    this.subscriptions.push(
+      this.productManagerFormService.getCategories().subscribe(
+        (response:Categories[])=>{
+          this.categories=response;
+          this.selected2=this.categories[1].categoryId
+      },
+      (error:HttpErrorResponse)=>{
+          console.log(error.message)
+      }
+    ));
+  }
+
+  onFileSelected($event:any) {
+     this.selectedFile = $event.target.files[0];
+     this.isNotPng=(this.selectedFile.type != 'image/png');
+  }
+
+  submit() {
+    this.isLoading = true;
+    const result = this.form.value
+    this.managerApiService.createProduct(this.selectedFile,result)
+      .pipe(
+        finalize(() => this.isLoading = false)
+      )
+      .subscribe( res => {
+        this.toaster.open({
+          text: Labels.product.success,
+          caption: Labels.caption.success,
+          duration: 4000,
+          type: 'success'
+        });
+      }, err => {
+        this.toaster.open({
+          text: Labels.product.error,
+          caption: Labels.caption.error,
+          duration: 4000,
+          type: 'danger'
+        });
+      })
+    if(this.form.valid) {
+      this.form.reset();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(
+      (subscriptions)=>subscriptions.unsubscribe()
+    );
+    this.subscriptions=[];
+  }
 }
