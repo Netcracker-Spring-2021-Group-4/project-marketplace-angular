ac10eb61ea6ee1fc3b2032cb8071816239908346
Fillip Molodtsov <greatdrummer1@gmail.com>
2021-06-25 12:55:50 +0300

    updating longText pipe and using it, using phonePipe

--- src/app/shared/helpers/pipes/long-text.pipe.ts
+++ src/app/shared/helpers/pipes/long-text.pipe.ts
@@ -5,12 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class LongTextPipe implements PipeTransform {
 
-  transform(value: string): string {
-    if(value.length < 350) return value
-    const untilFirstSpace = value.slice(340)
+  transform(value: string, max: number): string {
+    if(value.length < max) return value
+    const lineToStop = max - 10
+    const untilFirstSpace = value.slice(lineToStop)
     let idx = untilFirstSpace.indexOf(' ')
     idx = idx === -1 ? untilFirstSpace.length : idx
-    return value.slice(0, 340 + idx) + '...'
+    return value.slice(0, lineToStop + idx) + '...'
   }
 
 }
