59e33c7adaa8b41945c31d2a1a8aaa70ddd3877f
Ivan Mordas <ivanmordas49@gmail.com>
2021-06-28 09:20:29 +0300

    Deleted some imports and formated code

--- src/app/components/manager-plus/create-product-page/create-product-page.component.ts
+++ src/app/components/manager-plus/create-product-page/create-product-page.component.ts
@@ -1,48 +1,44 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormGroup} from "@angular/forms";
 import {ValidationMessages} from "../../../shared/models/labels/validation.message";
-import {Toaster} from "ngx-toast-notifications";
 import {finalize} from "rxjs/operators";
 import Labels from "../../../shared/models/labels/labels.constant";
 import {ManagerApiService} from "../../../api-services/manager-http.service";
-import {HttpErrorResponse} from "@angular/common/http";
 import {Subscription} from "rxjs";
 import {MatSelectChange} from "@angular/material/select";
 import {Category_DUBLICAT} from "../../../shared/models/api/receive/category_dublicat";
 import {ProductManagerFormService} from "../services/product-manager-form.service";
 import {ValidFile} from "../../../shared/components/file-uploader/file-uploader";
+import {ToasterCustomService} from "../../../services/toaster-custom.service";
 
 @Component({
   selector: 'app-create-product-page',
   templateUrl: './create-product-page.component.html',
   styleUrls: ['./create-product-page.component.scss']
 })
-export class CreateProductPageComponent implements OnInit,OnDestroy {
-
-  form: FormGroup
-  selectedFile?: File
+export class CreateProductPageComponent implements OnInit, OnDestroy {
+  imgUrl: any;
+  selectedFile?: File | undefined
+  isNotPng?: boolean = false
+  isHeavier?: boolean = false
+  isWrongResolution?: boolean = false;
   categories: Category_DUBLICAT[]
+  form: FormGroup
+  selected: any
   isLoading = false
-  currentValue:any
-  selected2:any
-  isNotPng?: boolean
-  isHeavier?:boolean
-  isWrongResolution?: boolean;
+  currentValue: any
   fileExpansionErrorMessage = ValidationMessages.expansion;
   fileWeightErrorMessage = ValidationMessages.weight;
   fileResolutionErrorMessage = ValidationMessages.resolution;
-  private subscriptions: Subscription[] = []
-  productNameErrorMessage =ValidationMessages.productName
+  productNameErrorMessage = ValidationMessages.productName
   quantityErrorMessage = ValidationMessages.quantity
   priceErrorMessage = ValidationMessages.price
-  imgUrl: any;
-
-
+  private subscriptions: Subscription[] = []
 
   constructor(
     private productManagerFormService: ProductManagerFormService,
     private managerApiService: ManagerApiService,
-    private toaster: Toaster,
+    private toaster: ToasterCustomService,
   ) {
     this.form = this.productManagerFormService.createProductForm();
   }
@@ -58,54 +54,41 @@ export class CreateProductPageComponent implements OnInit,OnDestroy {
     };
   }
 
-  public getCategories(): void{
+  public getCategories(): void {
     this.subscriptions.push(
       this.productManagerFormService.getCategories().subscribe(
-        (response:Category_DUBLICAT[])=>{
-          this.categories=response;
-          this.selected2=this.categories[1].categoryId
-        },
-        (error:HttpErrorResponse)=>{
-          console.log(error.message)
+        (response: Category_DUBLICAT[]) => {
+          this.categories = response;
+          this.selected = this.categories[1].categoryId
         }
-      ));
+      )
+    );
   }
 
   submit() {
     this.isLoading = true;
     this.form.patchValue({price: this.form.get('price')?.value * 100})
     const result = this.form.value
-    if(this.selectedFile) {
+    if (this.selectedFile) {
       this.managerApiService.createProduct(this.selectedFile, result)
         .pipe(
           finalize(() => this.isLoading = false)
         )
         .subscribe(res => {
-          this.toaster.open({
-            text: Labels.product.successfulCreationProduct,
-            caption: Labels.caption.success,
-            duration: 4000,
-            type: 'success'
-          });
+          this.toaster.successfulNotification(Labels.product.successfulCreationProduct);
         }, err => {
-          this.toaster.open({
-            text: err.error.message,
-            caption: Labels.caption.error,
-            duration: 4000,
-            type: 'danger'
-          });
+          this.toaster.errorNotification(err.error.message);
         })
     }
     this.imgUrl = undefined;
     this.form.reset();
-
   }
 
   ngOnDestroy(): void {
     this.subscriptions.forEach(
-      (subscriptions)=>subscriptions.unsubscribe()
+      (subscriptions) => subscriptions.unsubscribe()
     );
-    this.subscriptions=[];
+    this.subscriptions = [];
   }
 
   getFile(validFile: ValidFile) {
